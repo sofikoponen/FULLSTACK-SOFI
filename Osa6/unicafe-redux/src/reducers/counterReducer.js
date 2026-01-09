233155c56461482eaebbda84a2d76f474749edab
kopones3 <sofi.koponen@aalto.fi>
2026-01-09 14:09:54 +0200

    6.1-6.2

---
+++ Osa6/unicafe-redux/src/reducers/counterReducer.js
@@ -0,0 +1,38 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "GOOD": {
+      const goodIncremented = {
+        ...state,
+        good: state.good + 1,
+      };
+      return goodIncremented;
+    }
+    case "OK": {
+      const okIncremented = {
+        ...state,
+        ok: state.ok + 1,
+      };
+      return okIncremented;
+    }
+    case "BAD": {
+      const badIncremented = {
+        ...state,
+        bad: state.bad + 1,
+      };
+      return badIncremented;
+    }
+    case "RESET":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
